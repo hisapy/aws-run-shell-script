09cf188429d201a1d9bae0d4d1f3aa1671b72213
Hisa Ishibashi <hisa@iporaitech.com>
2026-02-23 12:49:55 -0300

    Update bundle

--- dist/index.js
+++ dist/index.js
@@ -42148,16 +42148,8 @@ async function run() {
             command: getInput('command', { required: true }),
             comment: getInput('comment'),
             user: getInput('user'),
-            timeoutSeconds: parseTimeout(getInput('timeout'), getBooleanInput('wait_for_result')),
-            dryRun: getBooleanInput('dry_run')
+            timeoutSeconds: parseTimeout(getInput('timeout'), getBooleanInput('wait_for_result'))
         };
-        if (inputs.dryRun) {
-            info('dry_run=true: skipping AWS SSM execution');
-            setOutput('command_id', 'dry-run');
-            setOutput('command_status', 'Success');
-            setOutput('command_output', 'dry_run=true: AWS SSM execution was skipped');
-            return;
-        }
         const client = new SSMClient();
         const command = await runShellScript(inputs, client);
         const outputs = await waitForResult(command, client, inputs.timeoutSeconds);
